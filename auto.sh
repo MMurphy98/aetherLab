767e34fa6f8a70aff6bc753de91ff5847e3ca23f
MMurphy98 <mjg654654@outlook.com>
2020-12-16 23:58:08 +0800

    First Commit

---
+++ auto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+# Here is comment 
+echo "Hello World!"
+
+GBW=108.39673M
+PM=62.863199
+echo $GBW
+echo $PM
+echo "GBW=${GBW}" > result.txt
+echo "PM=${PM}" >> result.txt
---
+++ read.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CAL_FLAG="calc"
+filename='/home/murphy/aetherlab/iwave.log'
+i=0
+while read line
+do
+    if [ ${line:0:4} == ${CAL_FLAG} ]
+    then
+        line_pro=${line#* }
+        case ${line_pro%%(*} in
+            "max")
+                echo "DC_GAIN: ${line##*:} dB" >> result.txt
+                let "i++"
+                echo $i 
+                ;;
+            "gain1_f")
+                echo "GBW: ${line##*:} Hz" >> result.txt
+                let "i++"
+                echo $i 
+                ;;                
+            "phase_m")
+                echo "PHASE_MARGIN: ${line##*:} deg" >> result.txt
+                let "i++"
+                echo $i 
+                ;;
+        esac
+    fi
+done < $filename
